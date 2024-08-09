b845a9a3566e768ad3db0349f541ffbbc71a31a9
AST <32876857+AST9QT@users.noreply.github.com>
2024-08-09 14:35:08 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+/**
+ * LED
+ */
+let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+moveMotorZIP.setColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+let led_bri = 255
+while (led_bri > 0) {
+    basic.pause(100)
+    moveMotorZIP.setBrightness(led_bri)
+    moveMotorZIP.show()
+    led_bri += -20
+}
+moveMotorZIP.setBrightness(0)
+moveMotorZIP.show()
 basic.forever(function () {
 	
 })
